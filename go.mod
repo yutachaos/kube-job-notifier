1a80ecfb2f90894f14f8c2ab69e50a43b1ea71ab
yutachaos <18604471+yutachaos@users.noreply.github.com>
2019-09-29 08:56:22 +0900

    Changed if running in a cluster, using to use kubeconfig in the cluster

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yutachaos/job-notify-controller
 go 1.13
 
 require (
+	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7
 	k8s.io/api v0.0.0-20190925180651-d58b53da08f5
 	k8s.io/apimachinery v0.0.0-20190925235427-62598f38f24e
 	k8s.io/client-go v0.0.0-20190925235746-07054768d98d
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/yutachaos/job-notify-controller/pkg/signals"
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog"
 	"os/user"
@@ -15,6 +16,7 @@ import (
 var (
 	masterURL  string
 	kubeconfig string
+	kubeClient kubernetes.Interface
 )
 
 func main() {
@@ -23,18 +25,21 @@ func main() {
 
 	stopCh := signals.SetupSignalHandler()
 
-	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
-	if err != nil {
-		klog.Fatalf("Error building kubeconfig: %s", err.Error())
-	}
-
-	kubeClient, err := kubernetes.NewForConfig(cfg)
-	if err != nil {
-		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
-	}
-
-	if err != nil {
-		klog.Fatalf("Error building example clientset: %s", err.Error())
+	if _, err := rest.InClusterConfig(); err != nil {
+		cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+		if err != nil {
+			klog.Fatalf("Error building example kubeclient: %s", err.Error())
+		}
+		kubeClient, err = kubernetes.NewForConfig(cfg)
+		if err != nil {
+			klog.Fatalf("Error building example kubeclient: %s", err.Error())
+		}
+	} else {
+		cfg, err := rest.InClusterConfig()
+		kubeClient, err = kubernetes.NewForConfig(cfg)
+		if err != nil {
+			klog.Fatalf("Error building in cluster kubeclient: %s", err.Error())
+		}
 	}
 
 	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
@@ -43,7 +48,7 @@ func main() {
 
 	kubeInformerFactory.Start(stopCh)
 
-	if err = controller.Run(2, stopCh); err != nil {
+	if err := controller.Run(2, stopCh); err != nil {
 		klog.Fatalf("Error running controller: %s", err.Error())
 	}
 }
